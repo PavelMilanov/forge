7483b9233666bcf1a632647f3d360289ca66b269
pavel_milanov <milanov71@icloud.com>
2025-04-15 22:16:03 +0300

    .

--- bin/docker/docker.go
+++ bin/docker/docker.go
@@ -3,3 +3,35 @@ package docker
 
 type Docker struct {
 }
+
+// func (d *Docker) command(command string, a ...string) error {
+// 	var cmd *exec.Cmd
+// 	switch command {
+// 	case "up":
+// 		if len(a) == 0 {
+// 			cmd = exec.Command("docker", "compose", "-f", c.File, "up", "-d")
+// 		} else {
+// 			cmd = exec.Command("docker", "compose", "-f", c.File, "up", "-d", strings.Join(a, " "))
+// 		}
+// 		cmd.Stdout = os.Stdout
+// 		cmd.Stderr = os.Stderr
+// 		if err := cmd.Run(); err != nil {
+// 			return fmt.Errorf("error %w", err)
+// 		}
+// 		return nil
+// 	case "down":
+// 		if len(a) == 0 {
+// 			cmd = exec.Command("docker", "compose", "-f", c.File, "down")
+// 		} else {
+// 			cmd = exec.Command("docker", "compose", "-f", c.File, "down", strings.Join(a, " "))
+// 		}
+// 		cmd.Stdout = os.Stdout
+// 		cmd.Stderr = os.Stderr
+// 		if err := cmd.Run(); err != nil {
+// 			return fmt.Errorf("error %w", err)
+// 		}
+// 		return nil
+// 	default:
+// 		return errors.New("неизвестная команда")
+// 	}
+// }
