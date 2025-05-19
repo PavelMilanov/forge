3c87f5199a3bf1c6bbc69a8feba293168e7eba65
Павел Миланов <pavel@MacBook-Pro-Pavel.local>
2025-05-19 15:15:14 +0300

    рефакторинг

--- bin/utils/templates_test.go
+++ bin/utils/templates_test.go
@@ -3,5 +3,12 @@ package utils
 import "testing"
 
 func TestGenerateAppConfig(t *testing.T) {
-	GenerateAppConfig("template.yml")
+	tags := map[string]string{
+		"test_alpine":   "test",
+		"test_nginx":    "test",
+		"test_postgres": "test",
+	}
+	if err := GenerateAppConfig("../docker/test/docker-compose.yaml", tags); err != nil {
+		t.Error(err)
+	}
 }
