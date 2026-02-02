ede8ffbadcf11445a9fca5199b203b17590532be
Pavel Milanov <milanov71@icloud.com>
2026-02-02 15:04:27 +0300

    добавлены методы api portainer

--- controller/api/portainer.go
+++ controller/api/portainer.go
@@ -1,10 +1,14 @@
 package api
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
+	"strconv"
 	"time"
 
 	"github.com/PavelMilanov/forge/config"
@@ -18,6 +22,13 @@ type Portainer struct {
 	Key string
 }
 
+type Stack struct {
+	ID       int    `json:"Id"`
+	Name     string `json:"Name"`
+	Endpoint int    `json:"EndpointId"`
+	File     string `json:"StackFileContent"`
+}
+
 // /api/stacks
 // /api/stacks/11/file
 // /api/stacks/11?endpointId=8
@@ -26,26 +37,104 @@ func NewPortainer(env *config.Env) (*Portainer, error) {
 	return &Portainer{Url: env.Portainer.Url, Key: env.Portainer.Key}, nil
 }
 
-func (p *Portainer) GetStacks() error {
+func (p *Portainer) GetStacks() ([]Stack, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, p.Url+"/api/stacks", nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	req.Header.Add("X-API-Key", p.Key)
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Printf("Error sending request: %s\n", err)
-		return err
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var stacks []Stack
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error reading response body: %s\n", err)
+		return nil, err
+	}
+	err = json.Unmarshal(body, &stacks)
+	if err != nil {
+		return nil, err
+	}
+	return stacks, nil
+}
+
+func (p *Portainer) GetStackFile(stack Stack) (*Stack, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, p.Url+"/api/stacks/"+strconv.Itoa(stack.ID)+"/file", nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("X-API-Key", p.Key)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Error sending request: %s\n", err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Printf("Error reading response body: %s\n", err)
-		return err
+		return nil, err
 	}
-	fmt.Println(string(body))
-	return nil
+	err = json.Unmarshal(body, &stack)
+	if err != nil {
+		return nil, err
+	}
+	return &stack, nil
+}
+
+func (p *Portainer) UpdateStack(stack *Stack) (string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	params := url.Values{}
+	params.Add("endpointId", strconv.Itoa(stack.Endpoint))
+
+	data := struct {
+		Env       []string `json:"env"`
+		Prune     bool     `json:"prune"`
+		PullImage bool     `json:"pullImage"`
+		File      string   `json:"stackFileContent"`
+	}{
+		Env:       []string{},
+		Prune:     true,
+		PullImage: true,
+		File:      stack.File,
+	}
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return "", err
+	}
+
+	requestBody := bytes.NewReader(jsonData)
+	req, err := http.NewRequestWithContext(ctx, http.MethodPut, p.Url+"/api/stacks/"+strconv.Itoa(stack.ID), requestBody)
+	if err != nil {
+		return "", err
+	}
+	req.URL.RawQuery = params.Encode()
+	req.Header.Add("X-API-Key", p.Key)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Error sending request: %s\n", err)
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error reading response body: %s\n", err)
+		return "", err
+	}
+	return string(body), nil
 }
--- controller/api/portainer_test.go
+++ controller/api/portainer_test.go
@@ -1,1 +1,67 @@
 package api
+
+import (
+	"testing"
+
+	"github.com/PavelMilanov/forge/config"
+)
+
+func TestGetStacks(t *testing.T) {
+	env, err := config.NewEnv("../var/forge", "forge")
+	if err != nil {
+		t.Errorf("Error loading environment: %s", err)
+	}
+	portainer, err := NewPortainer(env)
+	if err != nil {
+		t.Errorf("Error creating Portainer client: %s", err)
+	}
+	data, err := portainer.GetStacks()
+	if err != nil {
+		t.Errorf("Error getting stacks: %s", err)
+	}
+	t.Logf("%+v", data)
+}
+
+func TestGetStackFile(t *testing.T) {
+	env, err := config.NewEnv("../var/forge", "forge")
+	if err != nil {
+		t.Errorf("Error loading environment: %s", err)
+	}
+	portainer, err := NewPortainer(env)
+	if err != nil {
+		t.Errorf("Error creating Portainer client: %s", err)
+	}
+	stacks, err := portainer.GetStacks()
+	if err != nil {
+		t.Errorf("Error getting stacks: %s", err)
+	}
+	data, err := portainer.GetStackFile(stacks[0])
+	if err != nil {
+		t.Errorf("Error getting stack file: %s", err)
+	}
+	t.Logf("%+v", data)
+}
+
+func TestUpdateStack(t *testing.T) {
+	env, err := config.NewEnv("../var/forge", "forge")
+	if err != nil {
+		t.Errorf("Error loading environment: %s", err)
+	}
+	portainer, err := NewPortainer(env)
+	if err != nil {
+		t.Errorf("Error creating Portainer client: %s", err)
+	}
+	stacks, err := portainer.GetStacks()
+	if err != nil {
+		t.Errorf("Error getting stacks: %s", err)
+	}
+	data, err := portainer.GetStackFile(stacks[0])
+	if err != nil {
+		t.Errorf("Error getting stack file: %s", err)
+	}
+	resp, err := portainer.UpdateStack(data)
+	if err != nil {
+		t.Errorf("Error updating stack: %s", err)
+	}
+	t.Logf("%s", resp)
+}
