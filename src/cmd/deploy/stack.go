8db6109fdfa04b45b68a59cc536b654224c93db3
pavel_milanov <milanov71@icloud.com>
2026-02-13 20:36:13 +0300

    рефакторинг

--- src/cmd/deploy/stack.go
+++ src/cmd/deploy/stack.go
@@ -3,9 +3,6 @@ package deploy
 import (
 	"github.com/PavelMilanov/forge/agent"
 	"github.com/PavelMilanov/forge/errors"
-	"github.com/charmbracelet/bubbles/spinner"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 	"github.com/spf13/cobra"
 )
 
@@ -17,31 +14,16 @@ var stackCmd = &cobra.Command{
 	Args:      cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
 		cfg := agent.NewAgent()
-		// if deployTemplate != "" {
-		// 	if err := cfg.CreateStack(deployStack, deployTemplate); err != nil {
-		// 		errors.DeployErrors(err)
-		// 	}
-		// } else {
-		// 	if err := cfg.DeployStack(deployStack); err != nil {
-		// 		errors.DeployErrors(err)
-		// 	}
-		// }
-		s := spinner.New()
-		s.Spinner = spinner.Dot
-		s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
-
-		m := model{
-			spinner:  s,
-			cfg:      cfg,
-			stack:    deployStack,
-			template: deployTemplate,
+		if deployTemplate != "" {
+			if err := cfg.CreateStack(deployStack, deployTemplate); err != nil {
+				errors.DeployErrors(err)
+			}
+		} else {
+			if err := cfg.DeployStack(deployStack); err != nil {
+				errors.DeployErrors(err)
+			}
 		}
 
-		p := tea.NewProgram(m)
-
-		if _, err := p.Run(); err != nil {
-			errors.DeployErrors(err)
-		}
 	},
 }
 
